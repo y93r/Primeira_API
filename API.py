d623f9dff280e34b7d8749f2fda7961f348d8e09
Yara De Oliveira Rufino <104739868+y93r@users.noreply.github.com>
2023-12-16 17:19:23 -0300

    Update API.py

--- API.py
+++ API.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 #Construir as funcionalidades
 @app.route('/')
 def homepage():
-	arquivo = r'C:\Users\Usuário\Documents\Cursos\HashtagTreinamentos\Python_Impressionador\22-Integração Python com APIs e JSON\API\.venv\Criando API no Python.csv'
+	arquivo = r'C:\Users\Usuário\Documents\API\.venv\Criando API no Python.csv'
 	tabela = pd.read_csv(arquivo)
 	totais_colunas = tabela.sum().round(2).to_dict() #Calcula o total de cada coluna
 	return jsonify(totais_colunas)
